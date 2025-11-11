5c6069c87fcf7cacbb0fd82189d4b77fe942e859
Lkira80 <ana_es22@yahoo.es>
2025-11-11 12:22:13 +0100

    Add functionality to rename playlists

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Tracklist from "./Tracklist";
 
 function Playlist({ playlistName, setPlaylistName, playlistTracks, removeTrack }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [tempName, setTempName] = useState(playlistName);
+    
+    const handleTitleClick = () => {
+        setTempName(playlistName);
+        setIsEditing(true);
+    };
+
+    const handleInputChange = (e) => {
+        setTempName(e.target.value);
+    };
+
+    const handleInputBlur = () => {
+        setPlaylistName(tempName || "New Playlist");
+        setIsEditing(false);
+    };
+
+    const handleInputKeyPress = (e) => {
+        if (e.key === "Enter") {
+            handleInputBlur();
+        }
+    };
+    
     return (
         <div className="Playlist">
+           {isEditing ? ( 
             <input
-                value={playlistName}
-                onChange={(e) => setPlaylistName(e.target.value)}
+                autoFocus
+                value={tempName}
+                onChange={handleInputChange}
+                onBlur={handleInputBlur}
+                onKeyPress={handleInputKeyPress}
             />
+           ) : (
+            <h2 onClick={handleTitleClick}>{playlistName}</h2>
+           )}
             <Tracklist tracks={playlistTracks} removeTrack={removeTrack} isRemoval={true}/>
             <button>Save to Spotify</button>
         </div>
