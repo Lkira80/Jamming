66d03863506ea855311178596db5538344eec176
Lkira80 <ana_es22@yahoo.es>
2025-11-11 20:24:11 +0100

    Token removal

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -34,7 +34,6 @@ const Spotify = {
       return accessToken;
     } else {
       localStorage.removeItem("spotify_access_token");
-      localStorage.removeItem("spotify_token_expiry");
     }
 
     const code = new URLSearchParams(window.location.search).get("code");
