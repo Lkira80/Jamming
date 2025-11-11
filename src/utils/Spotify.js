e507c027810b8ff5158e4f785b067528a4b3d7bf
Lkira80 <ana_es22@yahoo.es>
2025-11-11 15:41:58 +0100

    Try to avoid err400

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -15,11 +15,11 @@ const Spotify = {
       expiresIn = Number(expiresInMatch[1]);
 
       window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
-      window.history.pushState("Access Token", null, "/");
+      window.history.pushState("Access Token", null, "/Jamming/");
 
       return accessToken;
     } else {
-      const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${encodeURIComponent(redirectUri)}`;
       window.location = authUrl;
     }
   },
