497c4bad3eec8a60fe3a3a05844ad3b3a3b8ed87
Lkira80 <ana_es22@yahoo.es>
2025-11-12 11:55:13 +0100

    Test 2.5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+    const [term, setTerm] = useState("");
+
+    const handleTermChange = (e) => {
+        setTerm(e.target.value);
+    };
+
+    const search = () => {
+        onSearch(term);
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") search();
+    };
+
     return (
        <div className="SearchBar">
-        <input placeholder="Enter a song, album or artist" />
-        <button>Search</button>
+        <input 
+        placeholder="Enter a song, album or artist" 
+        value={term}
+        onChange={handleTermChange}
+        onKeyDown={handleKeyPress}
+        />
+        <button onClick={search}>Search</button>
        </div>
     );
 }
